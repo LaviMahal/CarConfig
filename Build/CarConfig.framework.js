80475b9280a1eeca6ab2f1821797b15e216c67f6
Lavi_Mahal <lavi.mahal@gmail.com>
2021-01-28 17:52:50 +0530

    touch drag zone

--- Build/CarConfig.framework.js
+++ Build/CarConfig.framework.js
@@ -1300,7 +1300,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2970704;
+STATICTOP = STATIC_BASE + 2968896;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3278,7 +3278,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2970704;
+var STATIC_BUMP = 2968896;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15505,8 +15505,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 76111;
-Module["wasmMaxTableSize"] = 76111;
+Module["wasmTableSize"] = 75983;
+Module["wasmMaxTableSize"] = 75983;
 function invoke_dddi(index, a1, a2, a3) {
  var sp = stackSave();
  try {
